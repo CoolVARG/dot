3a19849aef30144b520952371680c585cc033fce
Timon <56367084+CoolVARG@users.noreply.github.com>
2022-03-12 16:47:04 +0300

    Update else_conf.sh

--- else_conf.sh
+++ else_conf.sh
@@ -5,3 +5,5 @@ sudo cp conf/default.conf /usr/lib/sddm/sddm.conf.d/default.conf
 sudo systemctl enable sddm.service
 
 sudo cp conf/environment /etc/environment
+
+sudo localedef -c -i ru_RU -f CP1251 ru_RU.CP1251
