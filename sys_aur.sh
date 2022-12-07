85f93814176668a2091525a8e419fc504e2a1a17
Timon <56367084+CoolVARG@users.noreply.github.com>
2022-12-07 19:56:36 +0300

    Update sys_aur.sh

--- sys_aur.sh
+++ sys_aur.sh
@@ -7,6 +7,7 @@ sudo pacman -U zst/otb-uw_ttyp0-1.3-1-any.pkg.tar.zst
 sudo pacman -U zst/redshift-wayland-git-1.12.r25.g7da875d-1-x86_64.pkg.tar.zst
 sudo pacman -U zst/swaylock-fancy-git-r220.265fbfb-1-any.pkg.tar.zst
 sudo pacman -U zst/ttf-clear-sans-1.00-3-any.pkg.tar.zst
+sudo pacman -U zst/qt5-styleplugins-5.0.0.20170311-33-x86_64.pkg.tar.zst
 
 sudo pacman -U zst/drivers/aic94xx-firmware-30-9-any.pkg.tar.zst
 sudo pacman -U zst/drivers/wd719x-firmware-1-7-any.pkg.tar.zst
